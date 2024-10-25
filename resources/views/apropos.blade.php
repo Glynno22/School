b93f634c769b6ceb16cd5d096b2555069a18aed6
Brayansmithe <tchoffobraya0@gmail.com>
2024-10-25 19:23:30 +0200

    mosification de temoignage

--- resources/views/apropos.blade.php
+++ resources/views/apropos.blade.php
@@ -7,7 +7,7 @@
      <!-- Header Start -->
      <div class="container-fluid bg-breadcrumb">
         <div class="container text-center py-5" style="max-width: 900px;">
-            <h3 class="text-white display-3 mb-4 wow fadeInDown" data-wow-delay="0.1s">About Us</h1>
+            <h3 class="text-white display-3 mb-4 wow fadeInDown" data-wow-delay="0.1s">À propos de nous</h1>
             <ol class="breadcrumb justify-content-center text-white mb-0 wow fadeInDown" data-wow-delay="0.3s">
                 <li class="breadcrumb-item"><a href="index.html" class="text-white">Home</a></li>
                 <li class="breadcrumb-item"><a href="#" class="text-white">Pages</a></li>
@@ -28,32 +28,32 @@
                     </div>
                 </div>
                 <div class="col-xl-7 wow fadeInRight" data-wow-delay="0.3s">
-                    <h5 class="sub-title pe-3">About the company</h5>
-                    <h1 class="display-5 mb-4">We’re Trusted Immigration Consultant Agency.</h1>
-                    <p class="mb-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt architecto consectetur iusto perferendis blanditiis assumenda dignissimos, commodi fuga culpa earum explicabo libero sint est mollitia saepe! Sequi asperiores rerum nemo!</p>
+                    <h5 class="sub-title pe-3">À propos de l'entreprise</h5>
+                    <h1 class="display-5 mb-4">Nous sommes une agence de conseil en immigration .</h1>
+                    <p class="mb-4"> Chez [nom de l'entreprise], nous facilitons votre transition de l'Afrique vers l'Europe en vous offrant des solutions d'immigration personnalisées de toutes natures. Notre objectif est de vous accompagner à chaque étape de votre parcours, en vous fournissant les informations et le soutien nécessaires pour réaliser vos rêves et bâtir un avenir meilleur, tout en vous ouvrant les portes de nouvelles opportunités.</p>
                     <div class="row gy-4 align-items-center">
                         <div class="col-12 col-sm-6 d-flex align-items-center">
                             <i class="fas fa-map-marked-alt fa-3x text-secondary"></i>
-                            <h5 class="ms-4">Best Immigration Resources</h5>
+                            <h5 class="ms-4">Les meilleures ressources en matière d'immigration</h5>
                         </div>
                         <div class="col-12 col-sm-6 d-flex align-items-center">
                             <i class="fas fa-passport fa-3x text-secondary"></i>
-                            <h5 class="ms-4">Return Visas Availabile</h5>
+                            <h5 class="ms-4">Procédure étudiant disponible</h5>
                         </div>
                         <div class="col-4 col-md-3">
                             <div class="bg-light text-center rounded p-3">
                                 <div class="mb-2">
                                     <i class="fas fa-ticket-alt fa-4x text-primary"></i>
                                 </div>
-                                <h1 class="display-5 fw-bold mb-2">34</h1>
-                                <p class="text-muted mb-0">Years of Experience</p>
+                                <h1 class="display-5 fw-bold mb-2">4</h1>
+                                <p class="text-muted mb-0"> années d'expérience</p>
                             </div>
                         </div>
                         <div class="col-8 col-md-9">
                             <div class="mb-5">
-                                <p class="text-primary h6 mb-3"><i class="fa fa-check-circle text-secondary me-2"></i> Offer 100 % Genuine Assistance</p>
-                                <p class="text-primary h6 mb-3"><i class="fa fa-check-circle text-secondary me-2"></i> It’s Faster & Reliable Execution</p>
-                                <p class="text-primary h6 mb-3"><i class="fa fa-check-circle text-secondary me-2"></i> Accurate & Expert Advice</p>
+                                <p class="text-primary h6 mb-3"><i class="fa fa-check-circle text-secondary me-2"></i> Offrir une assistance 100 % authentique</p>
+                                <p class="text-primary h6 mb-3"><i class="fa fa-check-circle text-secondary me-2"></i> C'est une exécution plus rapide et plus fiable</p>
+                                <p class="text-primary h6 mb-3"><i class="fa fa-check-circle text-secondary me-2"></i> Conseils précis et experts</p>
                             </div>
                             <div class="d-flex flex-wrap">
                                 <div id="phone-tada" class="d-flex align-items-center justify-content-center me-4">
@@ -65,8 +65,8 @@
                                     </a>
                                 </div>
                                 <div class="d-flex flex-column justify-content-center">
-                                    <span class="text-primary">Have any questions?</span>
-                                    <span class="text-secondary fw-bold fs-5" style="letter-spacing: 2px;">Free: +0123 456 7890</span>
+                                    <span class="text-primary">Avez-vous des questions?</span>
+                                    <span class="text-secondary fw-bold fs-5" style="letter-spacing: 2px;">+237658003716</span>
                                 </div>
                             </div>
                         </div>
